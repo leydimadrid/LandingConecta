a032144b960ce34639ffdb29dd5320a3fe3147c5
Leydi <leydi.madrid6@gmail.com>
2023-11-28 18:34:34 -0500

    Finalizada

--- src/components/ComponentesReutilizables/PlantillaRedesSociales.jsx
+++ src/components/ComponentesReutilizables/PlantillaRedesSociales.jsx
@@ -1,15 +1,10 @@
 import { useState } from "react";
 import Modal from "react-modal";
-import icono from "../../../public/imagen_DetallesTys.webp";
+import imagenGrande from "../../../src/assets/imagen_DetallesTys_grande.jpg";
 
 Modal.setAppElement("#root");
 
-export const PlantillaRedesSociales = ({
-  titulo,
-  imagen,
-  urlvistaprevia,
-  alt,
-}) => {
+export const PlantillaRedesSociales = ({ titulo, imagen, alt }) => {
   const [modalIsOpen, setModalIsOpen] = useState(false);
 
   const handleOpenModal = () => {
@@ -47,15 +42,20 @@ export const PlantillaRedesSociales = ({
           >
             Abrir imagen
           </button>
-            <Modal
-            className="w-auto h-auto flex justify-center place-items-center mt-56 bg-black"
-              isOpen={modalIsOpen}
-              onRequestClose={handleCloseModal}
-              contentLabel="Imagen más grande"
+          <Modal
+            className="modal md:w-2/4 lg:w-2/5 w-full flex flex-col gap-6 justify-center md:mt-48 mt-60 mx-auto px-4 py-4"
+            isOpen={modalIsOpen}
+            onRequestClose={handleCloseModal}
+            contentLabel="Imagen más grande"
+          >
+            <button
+              className="bg-purple-800 hover:bg-purple-900 font-semibold rounded-lg text-center text-white hover:scale-105 px-4 py-2 mx-auto cursor-pointer shadow-xl text-sm"
+              onClick={handleCloseModal}
             >
-              <button onClick={handleCloseModal}>Cerrar</button>
-              <img src={icono} descripcion="Descripción de la imagen" />
-            </Modal>
+              X
+            </button>
+            <img src={imagenGrande} />
+          </Modal>
         </div>
       </div>
     </div>
